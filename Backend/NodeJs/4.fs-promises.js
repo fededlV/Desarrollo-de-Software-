1a9e54c3b8433aa7c1a8e57512c8288920018d4a
fededlV <fededelavega2@gmail.com>
2024-04-28 22:37:52 -0300

    cambio cualquiera

--- Backend/NodeJs/4.fs-promises.js
+++ Backend/NodeJs/4.fs-promises.js
@@ -1,16 +1,16 @@
-const fs = require("node:fs/promises")
-const { text } = require("stream/consumers")
+const fs = require('node:fs/promises')
+const { text } = require('stream/consumers')
 
-console.log("Leyendo archivo 1...")
-fs.readFile("./archivo.txt", "utf-8")
+console.log('Leyendo archivo 1...')
+fs.readFile('./archivo.txt', 'utf-8')
     .then(text => {
-        console.log("Primer texto: ", text)
+        console.log('Primer texto: ', text)
     })
 
-console.log("----> Hacer mientras lee el archivo...")
+console.log('----> Hacer mientras lee el archivo...')
 
-console.log("Leyendo archivo 2...")
-fs.readFile("./archivo2.txt", "utf-8") 
-    .then (text => { 
-        console.log("Segundo texto: ", text)
+console.log('Leyendo archivo 2...')
+fs.readFile('./archivo2.txt', 'utf-8')
+    .then(text => {
+        console.log('Segundo texto: ', text)
     })
